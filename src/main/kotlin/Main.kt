d41da7e9459f75e911dd9a5d9b756eec1d9ecc43
Rémy LAVERGNE-PRUDENCE <lavergne.remy@gmail.com>
2019-10-18 00:14:05 +0200

    HTTP library

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World !")
+    doHttpCall()
+}
+
+fun doHttpCall() {
+
+    val url = "https://www2.yggtorrent.pe/engine/search?name=lion+king&description=&file=&uploader=&category=2143&sub_category=all&do=search"
+
+
+
+
 }
