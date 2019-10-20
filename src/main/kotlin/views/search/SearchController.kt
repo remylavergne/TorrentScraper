7455792c95c036d09fd51700e1334df3c3a23cb7
Rémy LAVERGNE-PRUDENCE <lavergne.remy@gmail.com>
2019-10-20 20:48:43 +0200

    Add tableview and show some data mocks

--- src/main/kotlin/views/search/SearchController.kt
+++ src/main/kotlin/views/search/SearchController.kt
@@ -1,8 +1,20 @@
 package views.search
 
+import models.YggTorrent
 import tornadofx.Controller
+import tornadofx.observable
 
 class SearchController : Controller() {
+
+    // Mock
+    var results = listOf(
+        YggTorrent(filename = "Le Seigneur des Anneaux", commentsCount = "12", seeders = "34", leechers = "46"),
+        YggTorrent(filename = "Le Seigneur des Anneaux", commentsCount = "12", seeders = "34", leechers = "46"),
+        YggTorrent(filename = "Le Seigneur des Anneaux", commentsCount = "12", seeders = "34", leechers = "46"),
+        YggTorrent(filename = "Le Seigneur des Anneaux", commentsCount = "12", seeders = "34", leechers = "46"),
+        YggTorrent(filename = "Le Seigneur des Anneaux", commentsCount = "12", seeders = "34", leechers = "46")
+    ).observable()
+        private set
 
     fun dailyFiles() {
 
--- src/main/kotlin/views/search/SearchView.kt
+++ src/main/kotlin/views/search/SearchView.kt
@@ -1,20 +1,37 @@
 package views.search
 
+import models.YggTorrent
 import tornadofx.*
 
 class SearchView : View() {
 
     private val controller: SearchController by inject()
 
-    override val root = hbox {
-        textfield("Value")
-        button("Search")
-        button("Today") {
-            action {
-                controller.dailyFiles()
-            }
-        }
+    override val root =
 
-    }
+        form {
+            vbox() {
+                text("Search")
+                hbox {
+                    textfield()
+                    button("Search")
+                }
+            }
+            hbox {
+                button("Today") {
+                    action {
+                        controller.dailyFiles()
+                    }
+                }
+            }
+
+            tableview(controller.results) {
+                readonlyColumn("ID", YggTorrent::id)
+                readonlyColumn("Name", YggTorrent::filename)
+                readonlyColumn("Added", YggTorrent::elapsedTimestamp)
+                readonlyColumn("Comments", YggTorrent::commentsCount)
+            }
+
+        }
 
 }
