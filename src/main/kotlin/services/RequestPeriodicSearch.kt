bf4bd28830be50dea99919d5a48501368188ff02
Rémy LAVERGNE-PRUDENCE <lavergne.remy@gmail.com>
2019-11-10 22:46:48 +0100

    Catch for Null Pointer Exceptions

--- src/main/kotlin/services/RequestPeriodicSearch.kt
+++ src/main/kotlin/services/RequestPeriodicSearch.kt
@@ -7,16 +7,24 @@ import java.io.File
 
 object RequestPeriodicSearch {
 
-    // TODO: Create a file by request.
+    /**
+     * This method override existing file with the same name
+     */
+    // TODO: Check if the file doesn't exist first
     fun saveARequest(request: String, results: List<Torrent>): Boolean {
         // Create Search Object
         val newRequest =
-            SavedRequest(request = request, allResults = results.map { it.url }, previousResultsHashcode = 0)
+            SavedRequest(request = request, allResults = results.map { it.url })
+        // Save locally
+        try {
+            val newFile = File(request.replace(' ', '-') + ".json")
+            newFile.writeText(newRequest.toJson())
+            return true
+        } catch (e: NullPointerException) {
+            // TODO: Get Stacktrace ?
+        }
 
-        val newFile = File(request.replace(' ', '-') + ".json")
-        newFile.writeText(newRequest.toJson())
-
-        return true
+        return false
     }
 
     fun deleteARequest(request: String) {
